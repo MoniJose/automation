daff299ec10463c9c6b3f09aff092a1635231632
josmi <monijose9048@gmail.com>
2021-04-24 17:28:06 +0530

    modified cod

--- dialysis_aut.py
+++ dialysis_aut.py
@@ -4,14 +4,14 @@ from time import sleep
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
+
 import urllib
 import urllib.request
 # functions
@@ -23,6 +23,12 @@ def check_exists_by_xpath(xpath):
         return False
     return True
 
+def check_exists_by_id(id):
+    try:
+        driver.find_element_by_id(id)
+    except NoSuchElementException:
+        return False
+    return True
 
 
 
@@ -94,6 +100,8 @@ for i in range(1,number_of_rows+1):
     patient_name = driver.find_element_by_xpath('//*[@id="out-table"]/div[1]/div/div[4]/div/div[3]/table/tbody/tr[1]/td[5]/div')
     p_name = patient_name.get_attribute('innerHTML')
     print(p_name)
+    # dep = driver.find_element_by_xpath('')
+    # treat_photo = dep.get_attribute('innerHTML')
 
     pulse_rate = driver.find_element_by_xpath(
         '/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/div/div[2]/div/div/div[3]/p[2]')
@@ -128,14 +136,6 @@ for i in range(1,number_of_rows+1):
         # urllib.urlretrieve(url, fullfilename)
         urllib.request.urlretrieve(url, fullfilename)
 
-
-
-    #
-    # def dl_img(url, file_path, file_name):
-    #     full_path = file_path + file_name + '.jpg'
-    #     urllib.request.urlretrieve(url, full_path)
-    #
-    #
     url = src1
 
     file_name = card
@@ -150,14 +150,14 @@ for i in range(1,number_of_rows+1):
 
     url = src2
 
-    file_name = card
+    file_name = p_name
 
     dl_img(url, r'C:\Users\PC\Desktop\certificate', file_name)
     sleep(2)
 
     url = src3
 
-    file_name = card
+    file_name = ip_num
 
     dl_img(url, r'C:\Users\PC\Desktop\chart', file_name)
     sleep(3)
@@ -180,7 +180,8 @@ def login_tms():
     # captcha = input("enter captcha here")
     captcha_text = driver.find_element_by_xpath('//*[@id="reqCaptcha"]')
     captcha_text.click()
-    input('Continue?  (Hit enter)')
+    sleep(15)
+    captcha_text.send_keys(Keys.ENTER)
     driver.find_element_by_id('checkSubmit').click()
     login = driver.find_element_by_xpath('//*[@id="login-submit"]')
     login.click()
@@ -236,7 +237,7 @@ try:
     sleep(5)
     print('try')
 except:
-    sleep(3)
+    sleep(5)
     # driver.find_element_by_xpath('html/body/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[1]/div/span/span[1]/span/span[1]').click()
 
 
@@ -275,7 +276,7 @@ try:
     add_pro.click()
     sleep(2)
 
-    ok =w.until(
+    ok = w.until(
         expected_conditions.presence_of_element_located((By.XPATH, '/html/body/div[26]/div/div/div[2]/button')))
     ok.click()
     sleep(2)
@@ -283,36 +284,79 @@ try:
 except:
     pass
 
+close_b = w.until(
+          expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="notify"]/div/div/div[3]/button')))
+
+close_b.click()
+sleep(3)
+seq = driver.find_elements_by_tag_name('iframe')
+
+for num, frame in enumerate(seq):
+    print(num, frame.get_attribute("id"))
+
+print("No of frames present in the web page are: ", len(seq))
+driver.switch_to.default_content()
+sleep(5)
+driver.switch_to.frame(driver.find_element_by_id('middleFrame'))
+sleep(5)
+
 
 try:
-
-    close_b = w.until(
-        expected_conditions.presence_of_element_located((By.XPATH, '/html/body/div[18]/div/div/div[3]/button')))
-
-    close_b.click()
-    sleep(3)
-    driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="iframe1"]'))
-    upload = driver.find_element_by_xpath('//*[@id="invAttach"]')
-    # path = os.path.join(r'C:\Users\PC\Desktop\certificate+' card '+.jpeg')
-    # upload.send_keys(path)
-    file_path =r'C:\Users\PC\Desktop\certificate'
-    filename = card + ".jpeg"
-    fullfilename = os.path.join(file_path, filename)
-    # urllib.urlretrieve(url, fullfilename)
-    driver.switch_to.default_content()
-    # driver.switch_to.frame(driver.find_element_by_id("middleFrame"))
-    sleep(2)
-
-
-
-    driver.switch_to.frame(driver.find_element_by_id('iframe2'))
-    upload_p = driver.find_element_by_xpath('//*[@id="invattachButton"]')
-    upload_p.send_keys(r'C:\Users\PC\Desktop\chart\card.jpeg')
+         driver.find_element_by_xpath('/html/body/div[26]/div/div/div[2]/button').click()
 except:
     pass
+driver.switch_to.default_content()
+sleep(1)
+driver.switch_to.frame(driver.find_element_by_id('middleFrame'))
+sleep(2)
+# driver.switch_to.frame(driver.find_element_by_id('iframe1'))
+if check_exists_by_id("iframe1") and check_exists_by_id("iframe2"):
+    # driver.switch_to.frame(driver.find_element_by_id('iframe1'))
+    # upload_p = driver.find_element_by_id('invattachButton')
+    # upload_p.send_keys(r'C:\Users\PC\Desktop\certificate\MUHAMMED ASHRAF .jpeg')
+    driver.switch_to.frame(driver.find_element_by_id('iframe1'))
+    upload = driver.find_element_by_xpath('//*[@id="invAttach"]')
+    file_path = r'C:\Users\PC\Desktop\chart'
+    filename = ip_num + ".jpeg"
+    fullfilename = os.path.join(file_path, filename)
+    upload.send_keys(fullfilename)
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame(driver.find_element_by_id('iframe2'))
+    upload_p = driver.find_element_by_xpath('//*[@id="invAttach"]')
+    file_path = r'C:\Users\PC\Desktop\certificate'
+    filename = p_name + ".jpeg"
+    fullfilename = os.path.join(file_path, filename)
+    upload_p.send_keys(fullfilename)
+
+
+elif check_exists_by_id("iframe0") and check_exists_by_id("iframe1"):
+    driver.switch_to.frame(driver.find_element_by_id('iframe0'))
+    upload = driver.find_element_by_xpath('//*[@id="invAttach"]')
+    file_path = r'C:\Users\PC\Desktop\chart'
+    filename = ip_num + ".jpeg"
+    fullfilename = os.path.join(file_path, filename)
+    upload.send_keys(fullfilename)
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame(driver.find_element_by_id('iframe1'))
+    upload_p = driver.find_element_by_xpath('//*[@id="invAttach"]')
+    file_path = r'C:\Users\PC\Desktop\certificate'
+    filename = p_name + ".jpeg"
+    fullfilename = os.path.join(file_path, filename)
+    upload_p.send_keys(fullfilename)
+
+    # # upload on iframe0 and then iframe1
+    # print("uploaded")
+else:
+    print("something went wrong")
+    exit()
+
+
+
+
 
 driver.switch_to.default_content()
 driver.switch_to.frame(driver.find_element_by_id("middleFrame"))
+
 
 ip_no = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[2]/div[1]/input')
 ip_no.send_keys(ip_num)
@@ -326,13 +370,6 @@ drop = Select(diagnosed_by)
 drop.select_by_visible_text('Others')
 doctor_name = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[3]/div[2]/div[1]/input')
 doctor_name.send_keys('DR PRADEEP K J')
-# procedure_con = driver.find_element_by_xpath('//*[@id="procedureConsent"]')
-# procedure_con.click()
-# MLC = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[4]/div[1]/input[2]')
-# MLC.click()
-# driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[3]/div[3]/input').click()
-# sleep(3)
-
 alldate = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[3]/div[3]/input')
 print(treatment_d)
 print(alldate)
@@ -344,14 +381,16 @@ MLC = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[4]/d
 MLC.click()
 sleep(5)
 
-# general_find = driver.find_element_by_xpath('//*[@id="ipButtonsDiv"]/button[1]')
+
+
+
 
 general_find = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div/div[8]/button[1]')
 top = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div/div[1]')
 driver.execute_script("return arguments[0].scrollIntoView(true);",top)
 sleep(10)
 general_find.click()
-sleep(30)
+sleep(20)
 
 temperature = driver.find_element_by_xpath('/html/body/div[8]/div/div/div[2]/form/div[2]/div/div/div[2]/div[1]/div[11]/div[1]/input')
 sleep(2)
@@ -377,16 +416,37 @@ bp_h = driver.find_element_by_name('BP1')
 bp_h.send_keys(y)
 sleep(5)
 w.until(
-        expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="generalExamsfindings"]/div[2]/button')))
+        expected_conditions.presence_of_element_located((By.XPATH, '/html/body/div[8]/div/div/div[2]/form/div[2]/div/div/div[2]/div[2]/button'))).click()
 
 
 sleep(5)
-save_ok = driver.find_element_by_xpath('/html/body/div[26]/div/div/div[2]/button')
+save_ok = w.until(
+        expected_conditions.presence_of_element_located((By.XPATH, '/html/body/div[27]/div/div/div[2]/button')))
 save_ok.click()
+
 sleep(2)
 close_save = driver.find_element_by_xpath('/html/body/div[8]/div/div/div[3]/button')
 close_save.click()
 sleep(2)
+
+sleep(5)
+pf_history = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div/div[8]/button[3]')
+topp = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div')
+driver.execute_script("return arguments[0].scrollIntoView(true);",topp)
+sleep(10)
+pf_history.click()
+sleep(20)
+not_applicable = driver.find_element_by_xpath('/html/body/div[8]/div/div/div[2]/form/div[1]/div[1]/div/div[2]/div[12]/div[1]/input')
+not_applicable.click()
+save_history = driver.find_element_by_xpath('/html/body/div[8]/div/div/div[2]/form/div[2]/button')
+save_history.click()
+sleep(3)
+ok_history = driver.find_element_by_xpath('/html/body/div[27]/div/div/div[2]/button')
+ok_history.click()
+sleep(2)
+close_history = driver.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/button')
+close_history.click()
+sleep(3)
 
 action_typ = driver.find_element_by_id('actionType')
 drop = Select(action_typ)
@@ -395,14 +455,74 @@ sleep(2)
 
 add_view = driver.find_element_by_id('btnattach')
 add_view.click()
-driver.switch_to.frame(driver.find_element_by_name('btframe'))
-sleep(5)
-driver.find_element_by_xpath('//*[@id="uploadBPM"]').send_keys(r'C:\Users\PC\Desktop\auto+card.jpeg')
+driver.switch_to.default_content()
+sleep(1)
+driver.switch_to.frame('middleFrame')
 sleep(2)
-close_pp = driver.find_element_by_xpath('//*[@id="modalattachDiv"]/div/div/div[3]/button')
-close_pp.click()
+driver.switch_to.frame(driver.find_element_by_id('modalattDivIframe'))
+p_photo = driver.find_element_by_xpath('//*[@id="BPM1"]')
+file_path = r'C:\Users\PC\Desktop\auto'
+filename = card + ".jpeg"
+full_filename = os.path.join(file_path, filename)
+p_photo.send_keys(full_filename)
+p_ok = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/button')
+p_ok.click()
+driver.switch_to.default_content()
+driver.switch_to.frame(driver.find_element_by_id('middleFrame'))
+p_close = driver.find_element_by_xpath('//*[@id="modalattachDiv"]/div/div/div[3]/button')
+p_close.click()
+
+# submit = driver.find_element_by_xpath('//*[@id="btnSubmit"]')
+# submit.click()
+# sleep(2)
+# QA_button = driver.find_element_by_xpath('/html/body/div[26]/div/div/div[2]/button[2]')
+# QA_button.click()
+# sleep(2)
+# Q1 = driver.find_element_by_xpath('//*[@id="triggerDetails"]/div/table/tbody/tr[1]/td[3]/label[1]/span[2]')
+# Q1.click()
+# Q2 = driver.find_element_by_xpath('//*[@id="triggerDetails"]/div/table/tbody/tr[2]/td[3]/label[1]/span[2]')
+# Q2.click()
+# Q3 = driver.find_element_by_xpath('//*[@id="triggerDetails"]/div/table/tbody/tr[3]/td[3]/label[1]/span[2]')
+# Q3.click()
+# Q4 = driver.find_element_by_xpath('//*[@id="triggerDetails"]/div/table/tbody/tr[4]/td[3]/label[1]/span[2]')
+# Q4.click()
+# Q5 = driver.find_element_by_xpath('//*[@id="triggerDetails"]/div/table/tbody/tr[5]/td[3]/label[1]/span[2]')
+# Q5.click()
+# sleep(3)
+# Q_ok = driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/button')
+# Q_ok.click()
+# Q_close = driver.find_element_by_xpath('//*[@id="questionaireModal"]/div/div/div[1]/button')
+# Q_close.click()
+# sleep(2)
+# intiate = driver.find_element_by_xpath('/html/body/div[26]/div/div/div[2]/button[2]')
+# intiate.click()
+# sleep(5)
+# case_id = driver.find_element_by_xpath('/html/body/form[1]/center[1]/font/div/div/div[2]/div/strong/b[2]')
+# id_value = case_id.get_attribute('innerHTML')
+# sleep(2)
+# sub_id = driver.find_element_by_xpath('//*[@id="generateCasePrintPage"]')
+# sub_id.click()
+# sleep(10)
+# case_sd = driver.find_element_by_xpath('//*[@id="childmenu3"]/li[2]/a/span[1]')
+# case_sd.click()
+# sleep(2)
+# case_no = driver.find_element_by_name('caseNo')
+# case_no.send_keys(id_value)
+# search_no = driver.find_element_by_xpath('//*[@id="preauthForApproval"]/div[3]/button[1]')
+# search_no.click()
+# sleep(3)
+# caseno = driver.find_element_by_xpath('//*[@id="no-more-tables"]/table/tbody/tr/td[2]/b/u/a')
+# caseno.click()
+# close_n = driver.find_element_by_xpath('//*[@id="notify"]/div/div/div[3]/button')
+# close_n.click()
 #
+# # authonticate
 #
-
-
-
+# covid_case = driver.find_element_by_xpath('//*[@id="covidNo"]')
+# covid_case.click()
+# sleep(1)
+# t_dr = driver.find_element_by_xpath('//*[@id="collapseTrtDr"]/div[1]/span/span[1]/span/span[2]')
+# t_dr.click()
+# driver.find_element_by_xpath('/html/body/span/span/span[1]/input').send_keys('Specialist')
+# specialist = driver.find_element_by_xpath('//*[@id="select2-docType-result-ib3j-D"]')
+# specialist.click()
